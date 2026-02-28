4541133a7b249a8b85c05d586f75e3848eb98c89
spark <hjvasava21@gmail.com>
2026-03-01 02:41:44 +0530

    Add files via upload

---
+++ Rock_paper_secissor.py
@@ -0,0 +1,47 @@
+import random
+
+choice=["rock","paper","scissors"]
+
+you=input("select any one [rock,paper,scissors]: ").lower()
+computer=random.choice(choice)
+
+print(f"you chose:{you}\ncomputer chose: {computer}")
+
+# If elif statment
+
+if(you==computer):
+    print("'it's tie'")
+
+elif(you=="rock" and computer=="paper"):
+    print("'You Lose!'")
+
+elif(you=="paper" and computer=="rock"):
+    print("'You Won!'")
+    print("congratulations🎉")
+
+elif(you=="rock" and computer=="scissors"):
+    print("'You Won!'")
+    print("congratulations🎉")
+
+elif(you=="scissors" and computer=="rock"):
+    print("'You Lose!'")
+
+elif(you=="paper" and computer=="scissors"):
+    print("'You Lose!'")
+    
+elif(you=="scissors" and computer=="paper"):
+    print("'You Won!'")
+    print("congratulations🎉")
+
+else:
+    print("Something went wrong! \n'please check speeling'")
+
+
+
+
+
+
+
+
+
+
